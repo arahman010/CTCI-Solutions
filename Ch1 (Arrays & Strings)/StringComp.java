84eb347417551171458ac511d50dda5b76134f91
Azizur Rahman <Sarzil@Azizurs-MacBook-Pro.local>
2017-03-19 21:59:44 -0400

    1.6 started

---
+++ Ch1 (Arrays & Strings)/StringComp.java
@@ -0,0 +1,34 @@
+// Date: 03/19/2017
+// Problem 1.6
+// Question: String Compression: Implement a method to perform basic string compression using the counts of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the "compressed" string would not become smaller than the original string, your method should return
+//  the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+// Assumption Made: None. (Note : You can assume the string has only uppercase and lowercase letters (a - z) )
+
+// Time Complexity --> O(n)
+// Space Complexity --> O(1)
+
+// Import necessary libraries
+import java.io.*;
+
+// Declare Class Name
+public class FileName {
+    
+    // Constructor
+    public FileName() {
+        
+    }
+    
+    
+    // Function which is the solution to the problem
+    public boolean funcName() {
+        
+    }
+    
+    public static void main(String args[]){
+        
+        FileName newClass = new FileName();
+        
+        System.out.println("Print Something");
+    }
+}
