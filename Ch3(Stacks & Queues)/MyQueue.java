4c5d8c00d51095b725f5fa77e8e9a5a7d4579d09
arahman010 <arahman010@citymail.cuny.edu>
2017-07-08 12:41:03 -0400

    Impelementing Queue

---
+++ Ch3(Stacks & Queues)/MyQueue.java
@@ -0,0 +1,29 @@
+// Date: 07/08/2016
+// My Queue Class implemented from book and will be used by other solutions.
+
+
+// Import necessary libraries
+import java.io.*;
+import java.util.*;
+
+// Declare Class Name
+public class MyQueue {
+    
+    // Constructor
+    public MyQueue() {
+        
+    }
+    
+    
+    // Function which is the solution to the problem
+    public boolean funcName() {
+    
+    }
+    
+    public static void main(String args[]){
+        
+        MyQueue newClass = new MyQueue();
+        
+        System.out.println("Print Something");
+    }
+}
