53ca76c67773211bc773d148f3c12cccdc75bc0e
arahman010 <arahman010@citymail.cuny.edu>
2017-07-08 12:55:38 -0400

    Queue implemented

--- Ch3(Stacks & Queues)/MyQueue.java
+++ Ch3(Stacks & Queues)/MyQueue.java
@@ -7,23 +7,78 @@ import java.io.*;
 import java.util.*;
 
 // Declare Class Name
-public class MyQueue {
-    
-    // Constructor
-    public MyQueue() {
-        
+public class MyQueue<T> {
+    private static class QueueNode<T> {
+        private T data;
+        private QueueNode<T> next;
+
+        public QueueNode(T data){
+            this.data = data;
+        }
     }
     
+    // No Custom MyQueue Constructor
+
+    private QueueNode<T> first;
+    private QueueNode<T> last;
+
+    //add
+    public void add(T item){
+        QueueNode<T> temp = new QueueNode<T>(item);
+        if (last != null){
+            last.next = temp;
+        }
+        last = temp;
+        if(first == null){
+            first = last;
+        }
+    }
+
+    //remove
+    public T remove(){
+        if(first == null){
+            throw new NoSuchElementException();
+        }
+        T firstData = first.data;
+        if(last == first){
+            last = null;
+        }
+        first = first.next;
+    }
+
+    //peek
+    public T peek(){
+        if(first != null){
+            return first.data;
+        }
+        throw new NoSuchElementException();
+    }
     
-    // Function which is the solution to the problem
-    public boolean funcName() {
+    //isEmpty
+    public boolean isEmpty(){
+        return first == null;
+    }
     
+    public void printQueue() {
+        QueueNode<T> temp = first;
+        while(temp != null){
+            System.out.print(temp.data);
+            System.out.print(" , ");
+            temp = temp.next;
+        }
+        System.out.println();
     }
     
     public static void main(String args[]){
         
-        MyQueue newClass = new MyQueue();
+        MyQueue<Integer> newClass = new MyQueue<Integer>();
         
-        System.out.println("Print Something");
+        newClass.add(1);
+        newClass.add(2);
+        newClass.add(3);
+        newClass.add(4);
+
+        newClass.printQueue();
+
     }
 }
